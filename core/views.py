ceee921c703cc76bafe9b0a232f25086c2009968
rohan <ab.rohan462@gmail.com>
2022-06-03 08:57:48 +0600

    rohan

--- core/views.py
+++ core/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from core.models import Product
+from core.models import Product,UserItem
 from .filters import OrderFilter
 from django.http import HttpResponse
+from django.db.models import Count, F, Value
+from django.db import connection
+
 
 @login_required
 def cart(request):
@@ -15,5 +18,27 @@ def cart(request):
 
 @login_required
 def dataupdate(request):
-     products = Product.objects.all()
-     return HttpResponse(products)
+      #cursor = connection['db.sqlite3'].cursor()
+      #user_products = Product.objects.raw("UPDATE core_product SET quantity =core_product.quantity-(SELECT quantity FROM core_useritem WHERE product_id = core_product.id) where EXISTS (SELECT quantity FROM core_useritem WHERE product_id = core_product.id)")
+      #cursor.execute("UPDATE core_product SET quantity =core_product.quantity-(SELECT quantity FROM core_useritem WHERE product_id = core_product.id) where EXISTS (SELECT quantity FROM core_useritem WHERE product_id = core_product.id)")
+     
+      with connection.cursor() as cursor:
+        cursor.execute("UPDATE core_product SET quantity =core_product.quantity-(SELECT quantity FROM core_useritem WHERE product_id = core_product.id) where EXISTS (SELECT quantity FROM core_useritem WHERE product_id = core_product.id)")
+        
+        row = cursor.fetchone()
+
+        
+      return render(request, 'core/a.html')
+                 
+				  
+				   
+
+                  
+                  
+
+
+
+      
+
+
+      
