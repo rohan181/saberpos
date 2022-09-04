1491e5136015b66b6fdf56df5ac610e339d3bcd2
rohan <ab.rohan462@gmail.com>
2022-09-04 13:14:38 +0600

    static

--- cart/settings.py
+++ cart/settings.py
@@ -116,8 +116,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATIC_ROOT = ''
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
